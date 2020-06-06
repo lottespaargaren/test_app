799a30c6c08a47a50a47e6613f7cea1058fcdee5
lottespaargaren <lottespaargaren@gmail.com>
2020-06-06 13:29:22 +0200

    Add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root 'pages#home' #where do we send this to, to a controller 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home' #where do we send this to, to a controller (pages controller) (action home)
+  get 'about', to: 'pages#' #get request to the about page, pages controller, about action
 end
